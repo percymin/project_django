97b4853150828d85b05c702e4727b5a11f418297
Seung Gi Min <sammymin@naver.com>
2022-02-20 01:23:18 +0900

    태그 페이지 만들기

--- blog/tests.py
+++ blog/tests.py
@@ -163,4 +163,22 @@ class TestView(TestCase):
         self.assertIn(self.post_001.title, main_area.text)
         self.assertNotIn(self.post_002.title, main_area.text)
         self.assertNotIn(self.post_003.title, main_area.text)
+
+    def test_tag_page(self):
+        response = self.client.get(self.tag_hello.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        self. assertIn(self.tag_hello.name, soup.h1.text) # 이페이지에서는 h1택가 한번만 나오기떄문에 여기서 확인
+
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.tag_hello.name, main_area.text)
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertNotIn(self.post_003.title, main_area.text)
+
+
 # Create your tests here.
