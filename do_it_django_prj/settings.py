f97adf38cc626066205ae8d8c23265b5e7addd0a
Seung Gi Min <sammymin@naver.com>
2022-04-08 18:08:46 +0900

    도커에 장고만 담기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*=qwh53nrc^v-zo)9te)#83u_i!!g_9c69&f9odi&ybz2qsj=a'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*=qwh53nrc^v-zo)9te)#83u_i!!g_9c69&f9odi&ybz2qsj=a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
